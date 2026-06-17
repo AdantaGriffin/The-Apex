0b034e6d941f256344f0d2a326455884ea1dca28
Your Name <you@example.com>
2026-06-17 12:40:24 -0400

    started on pc collection

--- src/components/collections/privateCollection.tsx
+++ src/components/collections/privateCollection.tsx
@@ -1,9 +1,27 @@
 import styles from './privateCollection.module.scss';
+import { Link } from 'react-router-dom';
+import { useApi } from '../api/api';
 
 function PrivateCollections(){
+    const {data} = useApi();
+    //console.log(data)
+    const pcData = data.filter(x => x.id.slice(0,2) === 'pc');
+    console.log(pcData)
     return(
         <>
-            <section className={styles.privateCollections}>P Collections</section>
+            <section className={styles.privateCollections}>
+                <div className={styles.pcHero}>Private collections hero</div>
+                <div className={styles.pcList}>
+                    {pcData.map(x => (
+                        <article key={x.id}>
+                            <Link to="null">
+                                <img src={x.image} height="100%" width="100%" alt="item image"/>
+                                <p>{x.name}</p>
+                            </Link>
+                        </article>
+                    ))}
+                </div>
+            </section>
         </>
     )
 };
